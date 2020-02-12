0a6a76354afc7241dbce543990c07758f19b8abb
gtothief <60219224+gtothief@users.noreply.github.com>
2020-02-12 12:34:38 +0800

    Add files via upload
    作为临时文件上传,未整合其他功能前暂不作为工程传入

---
+++ download.py
@@ -0,0 +1,34 @@
+import os
+import requests
+from flask import app
+import urllib
+
+
+@app.route('/ready',methods=['get', 'post'])
+def ready():
+    return 'ok'
+
+
+@app.route('/git',method = ['get','post'])
+def git():
+    filename = os.path.join(os.getcwd(),'redisIP.txt')
+    url = 'https://raw.github.com/ys007/CTPAgent/master/CTPAgentCode/redisIP.txt'
+
+    r = requests.get(url)
+
+    with open(filename, 'wb') as f:
+        f.write(r.content)
+
+    return 'ok'
+
+@app.route('/svn',methods = ['get','post'])
+def svn():
+    url='svnurl'
+    fp = urllib.urlopen(url)
+    data = fp.read()
+    print(data)
+#无svn环境,待后续测试
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001, host=socket.gethostbyname(socket.gethostname()))
