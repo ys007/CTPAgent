a079e6af4fa24d319742f71e66407551dbced6ea
lishuaiyin02 <1573551600@qq.com>
2020-02-24 21:09:09 +0800

    修改redis状态信息和概要信息的存储以及函数返回内容

--- CTPAgentCode/results.py
+++ CTPAgentCode/results.py
@@ -72,22 +72,21 @@ def readarguments():
 
 
 #save extracted data.json
-def savedata(id,timest,time_format,result,msg):
-    one = {"id": '1', "timest": "123123113", "datetime": "2010-01-01 19:12:12", "status": "200",'msg':"该用例执行成功"}
-    one['id']=id
+def update_redis_abstract(key_name,id,timest,time_format):
+    one ={"agentid": '1', "timest": "", "datetime": ""}
+    one['agentid']=id
     one['timest']=timest
     one['datetime']= time_format
-    one['status']=result
-    one['msg']=msg
    # mess1 = {'这里是定制的结果': '1'}
     #mess1['这里是定制的结果'] = number
     # two = {"msg": msg}
     # data = dict(one,**two)
-    # one.keys().encode("utf-8")
-    # one.values.encode("utf-8")
     jsonData = json.dumps(one,ensure_ascii=False,default=str)
+    r = redis.StrictRedis(host="127.0.0.1", db=0, password='1', decode_responses=True)
+    if r.exists(key_name):
+        r.delete(key_name)
+    r.set(key_name,jsonData)  # key值暂时先固定，如果有需要后边会进行改动
 
-    return jsonData
     # 不将结果存储到文件中，直接返回一个json格式的值
     # fileObject = open('data.json', 'w',encoding='utf-8')
     # fileObject.write(jsonData)
@@ -197,26 +196,25 @@ def saveEXCEL(filename,datalst,title=TITLE,style=DEFAULT_STYLE,**kwargs):
     book.close()
 
 # update redis
-def update_redis(key_name,owner):
+def update_redis_status(status,owner):  #redis 的状态信息 状态信息的key为ip：port
     r = redis.StrictRedis(host="127.0.0.1", db=0, password='1', decode_responses=True)
-    jsonredis={"type": "security", "subType": "nmap", "status": "running", "owner": owner}
-    jaondata=json.dumps(jsonredis)
-
-    if r.exists(key_name):
-        r.delete(key_name)
-    r.set(key_name,jaondata)#key值暂时先固定，如果有需要后边会进行改动
+    jsonredis={"type": "security", "subType": "nmap", "status": status, "owner": owner}
+    key_status="127.0.0.1:80"
+    if r.exists(key_status):
+        r.delete(key_status)
+    r.set(key_status,json.dumps(jsonredis))#key值暂时先固定，如果有需要后边会进行改动
     # print("redis data",r.get("data"))
 
-
-def results(ip,port,arguments,key):
+def results(ip,port,arguments,key_name):
+    json_data={"status": "","msg":"","file":"","key":key_name}
     path = readpath()
     nm = nmap.PortScanner(nmap_search_path=('nmap', path))
-    # results = nm.scan(ip, port, arguments)
     #暂时先写死，上边是通过读取文件的方法
     #nm = nmap.PortScanner(nmap_search_path=('nmap', r"C:\software\Nmap\nmap.exe"))
     results = nm.scan(ip, port, arguments)
     raw = nm.get_nmap_last_output()
-    saveraw(key_name,raw)  # 存储原先的数据为xml
+    json_data["file"]=raw
+    # saveraw(key_name,raw)  # 存储原先的数据为xml
     a = nm.command_line()
     # print(ip,port,arguments)
     # print(raw)
@@ -228,13 +226,14 @@ def results(ip,port,arguments,key):
     print("输出的info为",info)
     keys = list(info.keys())
     if keys[0] == 'error':
-        result = '500'# 代表nmap语句执行失败
+        json_data["status"] = '500'# 代表nmap语句执行失败
         msg=info[keys[0]]
         msg=msg[0]
+        json_data["msg"]=msg
         print ("msg是",msg)
     else:
-        result = '200' #代表nmap语句执行成功
-        msg='该用例执行成功'
+        json_data["status"] = '200' #代表nmap语句执行成功
+        json_data["msg"]='该用例执行成功'
         # 只有在执行成功的时候才会保存端口信息
         # data222 = open("rawdata.xml").read() #验证data222和raw的数据是一样的
         # print("shuchu",data222)
@@ -261,24 +260,25 @@ def results(ip,port,arguments,key):
     # print(info)
     # print(keys[0])
     # print(results)
-    id = '2'
+    id = '1'
     GMT_FORMAT = "%a %b %d %H:%M:%S %Y"
     timest = results['nmap']['scanstats']['elapsed']
     datetime1 = results['nmap']['scanstats']['timestr']
     time_format1 = datetime.strptime(datetime1, GMT_FORMAT)
-    print(time_format1)
-
-    owner="1"
-    update_redis(key_name,owner)  # 将数据存入redis
-    jsondata=savedata(id, timest, time_format1, result, msg)  # 存储一些自己想要的结果
-    return jsondata
+    # print(time_format1)
+    owner="1" #由服务端传入
+    status="running"
+    update_redis_status(status,owner)  # 将数据存入redis
+    update_redis_abstract(key_name,id,timest, time_format1)  # 存储一些概要信息
+    return json.dumps(json_data,ensure_ascii=False,default=str) #返回值
 if __name__ == '__main__':
 
     ip='127.0.0.1'
     port='80-89'
     arguments='-Pn -sS -A'
-    key_name="1_data"
-    results(ip,port,arguments,key_name)
+    key_name="1_data"  #这个key是对应的redis的概要信息
+    re=results(ip,port,arguments,key_name)
+    print(re)
 
 
 
