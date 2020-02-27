fe9489084f6217029db4c0760d5bec2a2d17f465
cjj-work <yezicjj@163.com>
2020-02-27 16:06:34 +0800

    增加向redis发送状态，及个接口的调用部分

--- CTPAgentCode/doTest.py
+++ CTPAgentCode/doTest.py
@@ -1,8 +1,10 @@
 import yaml
 import os
-from results import results
-from monitorClient import yamlConfigFileHandle
+#from results import results
+from monitorClient import yamlConfigFilePath, yamlConfigFileHandle
+from download import svn
 import globalvar as gl
+from results import results
 
 try:
     from yaml import CLoader as Loader, CDumper as Dumper
@@ -40,10 +42,15 @@ def DoTest():
     print("data_type:", type(data))
     print("data_content:\n", data)
     #print("download_pwd", data['download']['password'])
-    execYamlConfig(data)
+    return execYamlConfig(data)
 
 #按照yaml文件中的配置项逐个执行，当前包括从配置库下载工具、执行nmap进行扫描
 def execYamlConfig(list_a):
+    status = '500' #设置默认值为500，表示执行失败
+    msg = '用例执行失败'
+    resultFile = '' #执行结果的具体内容，用于返回给服务端使用
+    time = ''  #nmap工具执行一条命令所用的时间，这个是命令执行后返回的
+
     for i in list_a:
         print("list[%s]=" % i)
 
@@ -53,7 +60,29 @@ def execYamlConfig(list_a):
                 mode = i['download']['mode']
                 url = i['download']['url']
                 print('此处调用才中宝写的代码，此代码需在成功后返回ok，当判断返回值为ok后才能执行后续的代码')
-                break
+                setting = {
+                    # svn 的本地安装路径
+                    'svn': 'C:\\Program Files\\TortoiseSVN\\bin',
+                    # 需要下载的svn文件
+                    "url": url,
+                    # svn账号
+                    "user": 'caizhongbao',
+                    # svn密码
+                    "pwd": 'caizhongbao',
+                    # 下载到的路径
+                    "dist": yamlConfigFilePath
+                }
+                # os.rename(image, new_file)
+
+                ret = svn(setting=setting)  # 这个函数目前没有返回值，需要加返回值，如果确实下载下来了文件，返回ok，否则返回false
+                if ret == 'ok':
+                    break
+
+                else:
+                    print('从配置库获取工具失败，无法执行后续测试！')
+
+                    return {"status": '500', "msg": "从配置库获取工具失败，无法执行后续测试！", "file": '', "key": gl.get_value('key')}
+
             if k == 'test':
                 mode = i['test']['mode']
                 if mode == 'nmap':
@@ -63,13 +92,14 @@ def execYamlConfig(list_a):
                     arguments = i['test']['arguments']
 
                     # 此函数当前的返回值为执行结果，需要改一下，返回是否执行成功，当前返回的值作为全局变量使用
-                    ret = results(ip, port, arguments, gl.get_value('key2'))
+                    status, msg, resultFile, time = results(ip, port, arguments)
+
                 break
             else:
                 print(k)
                 break
-    gl.set_value('testResult', ret)  # 设置变量值 testResult，用于发送到redis
-    return gl.get_value('testResult')
+
+    return {"status": status, "msg": msg, "file": resultFile, "key": gl.get_value('key')}
 
 #if __name__ == '__main__':
 #    getConfiger()
--- CTPAgentCode/monitorClient.py
+++ CTPAgentCode/monitorClient.py
@@ -20,6 +20,11 @@ gl.set_value('key2', '0')  # 设置变量值 key2 = '0'
 executor = ThreadPoolExecutor()
 app = Flask(__name__)
 flag = 1
+
+#本地IP
+agentIP = socket.gethostbyname(socket.gethostname())
+#本程序运行端口号
+agentPort = '5001'
 #key = ''
 
 # 获取当前脚本所在文件夹路径，这个路径是文件夹的路径，不带文件名
@@ -82,32 +87,54 @@ def readIP():
 
 #begin add by cjj
 
+def pushStatusIdle():
+    r = redis.StrictRedis(host=readIP()[0], db=0, password=readIP()[1], decode_responses=True)
+    payload = {'type': 'security', 'subType': 'nmap', 'status': 'ready', 'owner': ''}
+    temp = json.dumps(payload)
+    statusKey = agentIP + ':' + agentPort
+    r.lpush(statusKey, temp)
+    print(temp)
+
+pushStatusIdle()
 
 #准备接口：agent去指定配置库中获取yaml测试文件【文件url服务端会传入】，以及agent自身需要准备的任务。
 @app.route('/prepare', methods=['get', 'post'])
 def prepare():
+    global owner
     jsonstr = request.json
-    url = jsonstr.get("fileurl")
+    fileurl = jsonstr.get("fileurl")
+    repository = jsonstr.get("repository")
+    owner = jsonstr.get("owner")
 
     #下载模式字段，需要根据和服务端的调试情况来调整
-    mode = "git"
+    if repository == 'svn':
+        #这里调用才中宝的代码下载yaml文件，文件下载后的保存路径为yamlConfigFilePath
+        setting = {
+            # svn 的本地安装路径
+            'svn': 'C:\\Program Files\\TortoiseSVN\\bin',
+            # 需要下载的svn文件
+            "url": fileurl,  #'https://39.97.104.82/svn/CTPDemo/gitUser.txt'  #这个地方需要和服务端确认，文件的名字是在url中传过来还是一个单独的参数？？？？？？？
+            # svn账号
+            "user": 'caizhongbao',
+            # svn密码
+            "pwd": 'caizhongbao',
+            # 下载到的路径
+            "dist": yamlConfigFilePath
+        }
+        #os.rename(image, new_file)
 
-    #这里调用才中宝的代码下载yaml文件，文件下载后的保存路径为yamlConfigFilePath
-    setting = {
-        # svn 的本地安装路径
-        'svn': 'C:\\Program Files\\TortoiseSVN\\bin',
-        # 需要下载的svn文件
-        "url": url,  #'https://39.97.104.82/svn/CTPDemo/gitUser.txt'  #这个地方需要和服务端确认，文件的名字是在url中传过来还是一个单独的参数？？？？？？？
-        # svn账号
-        "user": 'caizhongbao',
-        # svn密码
-        "pwd": 'caizhongbao',
-        # 下载到的路径
-        "dist": yamlConfigFilePath
-    }
-    svn(setting = setting)  #这个函数目前没有返回值，需要加返回值，如果确实下载下来了文件，返回ok，否则返回false
 
-    #os.rename(image, new_file)
+        ret = svn(setting = setting)  #这个函数目前没有返回值，需要加返回值，如果确实下载下来了文件，返回ok，否则返回false
+        if ret == 'ok':
+            r = redis.StrictRedis(host=readIP()[0], db=0, password=readIP()[1], decode_responses=True)
+            payload = {'type': 'security', 'subType':'nmap', 'status':'ready', 'owner': owner}
+            temp = json.dumps(payload)
+            statusKey = agentIP + ':' + agentPort
+            r.lpush(statusKey, temp)
+            print(temp)
+            return {"status": '200', "msg": '执行成功'}
+        else:
+            return {"status": '200', "msg": '执行成功'}
 
 
 #执行接口：agent根据之前获取的yaml测试文件执行测试并将测试结果写入redis【redis的key由服务端传入】
@@ -118,44 +145,59 @@ def run():
     global flag
     flag = 1
     jsonstr = request.json
-    gl.set_value('key1', jsonstr.get("key1"))  # 设置变量值 key1的值
-    gl.set_value('key2', jsonstr.get("key2"))  # 设置变量值 key2的值
+    gl.set_value('key', jsonstr.get("key"))  # 设置变量值 key的值
+
+    #开始执行测试，向redis发送running状态
     global r
     r = redis.StrictRedis(host=readIP()[0], db=0, password=readIP()[1], decode_responses=True)
-    r.delete(gl.get_value('key1'))
-    r.delete(gl.get_value('key2'))
-    print("开始执行")
-    executor.submit(do_update)
-    return 'ok'
+    payload = {'type': 'security', 'subType': 'nmap', 'status': 'running', 'owner': owner}
+    temp = json.dumps(payload)
+    statusKey = agentIP + ':' + agentPort
+    r.lpush(statusKey, temp)
+    #r.delete(gl.get_value('key'))
+    print("开始执行测试")
+    task = executor.submit(do_update)
+
+    #向redis发送实时状态，测试执行完，实时状态变为idle
+    payload = {'type': 'security', 'subType': 'nmap', 'status': 'idle', 'owner': ''}
+    temp = json.dumps(payload)
+    statusKey = agentIP + ':' + agentPort
+    r.lpush(statusKey, temp)
+    return task.result()
 
 
 def do_update():
     global flag, r
-    r = redis.StrictRedis(host=readIP()[0], db=0, password=readIP()[1], decode_responses=True)
+    #r = redis.StrictRedis(host=readIP()[0], db=0, password=readIP()[1], decode_responses=True)
     while True:
         if flag == 0:
             break
         #time.sleep(res)
         #下面这几句是从李帅印的代码中得到发送给redis的内容
-        ret = doTest.DoTest()
-        if ret == 'ok':
-            temp = json.dumps(gl.get_value('testResult'))
-            r.lpush(gl.get_value('key2'), temp)
-            print(temp)
+        return doTest.DoTest()
 
 #取消接口：agent删除之前获取的yaml测试文件。
 app.route('/cancel', methods=['get', 'post'])
 def cancel():
-
+    #1、删除yaml文件
     if os.path.exists(yamlConfigFileHandle):  # 如果文件存在
         # 删除文件
         os.remove(yamlConfigFileHandle)
-        return 'ok'
+
+        #2、上报本机状态
+        r = redis.StrictRedis(host=readIP()[0], db=0, password=readIP()[1], decode_responses=True)
+        payload = {'type': 'security', 'subType':'nmap', 'status':'idle', 'owner': ''}
+        temp = json.dumps(payload)
+        statusKey = agentIP + ':' + agentPort
+        r.lpush(statusKey, temp)
+        print(temp)
+        return {"status": '200', "msg": '执行成功'}
+
     else:
         print('no such file:%s' %yamlConfigFileHandle)  # 否则，返回文件不存在
-        return 'no such file'
+        return {"status": '500', "msg": '删除失败：没有测试yaml文件'}
     
 #end add by cjj
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001, host=socket.gethostbyname(socket.gethostname()))
+    app.run(debug=True, port=agentPort, host=agentIP)
